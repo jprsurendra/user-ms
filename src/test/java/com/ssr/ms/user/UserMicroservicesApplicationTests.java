ed69841d40d14a3a9295ce274cba168cd7cee928
Surendra <jpr_surendra@yahoo.co.in>
2022-12-25 19:53:09 +0530

    Project Configuration

---
+++ src/test/java/com/ssr/ms/user/UserMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssr.ms.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
